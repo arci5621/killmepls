b6fac814b2bb482af6c0d912e4f212c4ad3519f7
arci5621 <81012472+arci5621@users.noreply.github.com>
2021-03-20 10:46:02 +0100

    My very first commit from IDE

--- main.py
+++ main.py
@@ -12,8 +12,6 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
+    print('Hey you!')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
 
-# New comment and new functionality
-   print('Hey you!')
